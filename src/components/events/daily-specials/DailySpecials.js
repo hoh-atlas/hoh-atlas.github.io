69761a8705640e9d7853486b2cb5e70c3a17103c
davidvach <vachalek.david@gmail.com>
2024-12-14 18:00:27 +0100

    Small typo

--- src/components/events/daily-specials/DailySpecials.js
+++ src/components/events/daily-specials/DailySpecials.js
@@ -50,7 +50,7 @@ const DailySpecials = (props) => {
 
             <div style={{ textAlign: 'center', marginTop: '10px' }}>
                 <small style={{ fontSize: '11px', color: '#916a17' }}>
-                    <i>Note: The rewards offered by these customizations vary depending on your current era. Be sure to set your era on the website using the dropdown menu in the top-left corner.</i>
+                    <i>Note: Daily specials vary depending on your current era. Be sure to set your era on the website using the dropdown menu in the top-left corner, to see the accurate data for your current in-game progress.</i>
                 </small>
             </div>
 
