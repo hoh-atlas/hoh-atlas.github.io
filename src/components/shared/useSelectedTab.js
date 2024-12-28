06e46e8bbd05c8b43a813b66188284a2c5e96afc
davidvach <vachalek.david@gmail.com>
2024-12-28 13:35:45 +0100

    Migration to nextjs

--- src/components/shared/useSelectedTab.js
+++
@@ -1,8 +0,0 @@
-import { useLocation } from "react-router-dom";
-
-const useSelectedTab = () => {
-    let location = useLocation();
-    return "/" + location.pathname.split("/")[2];
-}
-
-export default useSelectedTab;
